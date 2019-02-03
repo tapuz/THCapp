fb44518ff85bb1ce288f9dbe17726704eaf74661
Thierry <thierry.duhameeuw@gmail.com>
2019-02-03 14:02:28 +0100

    version 0.0.2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,20 +2,33 @@ import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { PictureproofPage } from '../pictureproof/pictureproof';
 import { DocscanPage } from '../docscan/docscan';
+import io from 'socket.io-client';
+import * as Config from '../../config';
 
 
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+ 
 })
 export class HomePage {
-  //var declaration
+  socket:any;
+  temp:any;
 
   constructor(
     public navCtrl: NavController, 
     )
-    {}
+    {
+      this.socket = io(Config.THCServer);
+      this.socket.on("temp", (temp) => {
+        console.log('niets!!');
+          this.temp=temp;
+                 //console.log(message);
+      });  
+
+
+    }
 
   navToPictureProof(): void {
     this.navCtrl.push(PictureproofPage);
